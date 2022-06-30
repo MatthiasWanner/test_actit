11e999b866449bb029bcde896719fe6df7ea5115
MatthiasWanner <matthias.wanner@gmail.com>
2022-06-30 23:06:05 +0200

    :lipstick: (components) Create Bubble reusable component

---
+++ src/components/RandomBubble.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+interface IProps {
+  color: string;
+  size: string;
+}
+defineProps<IProps>();
+</script>
+
+<template>
+  <div :class="`rounded-full ${size} ${color}`"></div>
+</template>
