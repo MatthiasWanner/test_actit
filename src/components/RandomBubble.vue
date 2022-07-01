85ea352371e3708dbe8f18aec40cae8b7b6a7552
MatthiasWanner <matthias.wanner@gmail.com>
2022-07-01 21:44:37 +0200

    :sparkles: (feat) Dispatch bubbles randomly and dynamically on the viewport

--- src/components/RandomBubble.vue
+++ src/components/RandomBubble.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import type { IBubbleProps } from '../types';
+import { onMounted, ref } from 'vue';
+
+import { getBubbleId, getRandomInt } from '../utils';
 
 // Just use IBubbleProps interface throw vite compiler error
 interface IProps {
@@ -8,9 +11,24 @@ interface IProps {
   size: IBubbleProps['size'];
 }
 
-defineProps<IProps>();
+const props = defineProps<IProps>();
+
+const xPosition = ref<number>(getRandomInt(0, window.innerWidth + 1));
+const yPosition = ref<number>(getRandomInt(0, window.innerHeight + 1));
+const componentId = ref<string>(getBubbleId(props, getRandomInt(100, 1000000)));
+
+onMounted(() => {
+  const element = document.querySelector(`.${componentId.value}`);
+
+  if (element) {
+    (element as HTMLBodyElement).style.top = `${yPosition.value}px`;
+    (element as HTMLBodyElement).style.left = `${xPosition.value}px`;
+  }
+});
 </script>
 
 <template>
-  <div :class="`rounded-full bg-${color}-${nuance} w-${size} h-${size}`"></div>
+  <div
+    :class="`${componentId} absolute rounded-full bg-${color}-${nuance} w-${size} h-${size}`"
+  ></div>
 </template>
